d707aa55a66d2d040e58f788278aa9c2a277e121
Harsh Raj <78961911+Harshraj77@users.noreply.github.com>
2022-09-22 19:07:12 +0530

    added sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,25 @@
-import { useState, useEffect } from 'react'
-im 
-
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material' //mui/material
+import Sidebar from './Sidebar'
+import Videos from './Videos';
 const Feed = () => {
   return (
     <div>
+      <Stack sx={{flexDirection: {sx: "column", md:"row"}}}>
+        <Box sx={{height: {sx:'auto',md:'92vh'}, borderRight: "1px solid #3d3d3d", px:{ sx:0, md:2}}}>
+         <Sidebar/>
+         <Typography classname="copyright" varaint="body2" sx={{ mt:1.5, color:"#fff"}}>
+          Copyright 2022 Harsh Raj
+         </Typography>
+        </Box>
+        <Box p={2} sx={{overflowY:'auto',height: '90vh', flex:2}}> 
+          <Typography variant="h4" fontweight="bold" mb={2} sx={{color:'white'}}>
+            New <span style={{color:'#F31503'}}>Videos</span>
+          </Typography>
 
+          <Videos/>
+        </Box>
+      </Stack>
     </div>
   )
 }
